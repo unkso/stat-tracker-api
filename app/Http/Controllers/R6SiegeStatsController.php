1270ff36d8465829710cda907cc4349ddee9126e
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-02-25 12:31:10 -0500

    Removed unsused code. Updated docs.

--- app/Http/Controllers/R6SiegeStatsController.php
+++ app/Http/Controllers/R6SiegeStatsController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\concerns\BfStatsHelper;
-use App\Http\Controllers\concerns\BfStatsResponseHelper;
 use App\Http\Controllers\concerns\R6SiegeStatsHelper;
 use App\Http\Controllers\concerns\R6SiegeStatsResponseHelper;
 use Illuminate\Database\DatabaseManager;
