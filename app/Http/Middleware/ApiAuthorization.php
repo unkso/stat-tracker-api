bfa4d095f1cbd4df5409a64c5b5e758d493c922b
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-02-28 00:20:50 -0500

    Enabled auth middleware

--- app/Http/Middleware/ApiAuthorization.php
+++ app/Http/Middleware/ApiAuthorization.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Response;
 class ApiAuthorization
 {
     public function handle(Request $request, $next) {
-        // @todo - uncomment to enable auth.
-        /*
         $expectedToken = "Bearer 3C2B19E2946893CBE1AA14A7023867DAFDA0D4F1EEA9D4FF9C54EB4D09074C2B";
         $authHeader = $request->header('Authorization');
         if (empty($authHeader) || $authHeader != $expectedToken) {
@@ -17,7 +15,6 @@ class ApiAuthorization
                 "content-type" => "application/json"
             ]);
         }
-        */
 
         return $next($request);
     }
